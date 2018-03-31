c7f23109a6a2f512b85c9707e7307c38306cdbee
Nicusor97 <nicolaepicatureanu@gmail.com>
2018-03-31 06:05:46 -0700

    Create home page for online shopping boutique

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/main.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <title>Rainbow Style Boutique</title>
+</head>
+<body>
+    <!-- Top Nav Bar-->
+  <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <a href="index.php" class="navbar-brand">Rainbow Style Boutique</a>
+        <ul class="nav navbar-nav">
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Men<span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="#">Shirts</a></li>
+                <li><a href="#">Pants</a></li>
+                <li><a href="#">Shoes</a></li>
+                <li><a href="#">Accesories</a></li>
+              </ul>
+            </li>
+        </ul>
+    </div>
+  </nav>
+  <!-- Header -->
+  <div id="headerWrapper">
+    <div id="back-flower"></div>
+    <div id="logotext"></div>
+    <div id="fore-flower"></div>
+  </div>
+
+  <p>
+  Guess (clothing)
+    <p>
+In the 1990s, Milica, Aleksia and Milos[17] had a division called Guess Home, which featured youthful, upscale bedding collections (Guess was the first company to package each sheet, duvet and pillowcase pair in packaging actually made from sheeting material, which was a clever way to showcase what the pattern really looked like) as well as a number of innovative and unique towel collections. By the end of the decade, sales dropped and Guess discontinued their home division.[18]
+
+Improved sales
+In the 2000s, the controversy that surrounded the company during the nineties (see below) was largely forgotten. Guess product began catching the eye of many teens and young adults during the decade as its marketing ads grew increasingly sexier. Since mid-2003, the Guess stock has continuously risen, eliciting nothing but positive reviews from various stock holders and Wall Street, though the wider community has more mixed opinions. Recently, the clothing and accessories company has redesigned itself, offering several new aspects.[19]
+
+
+Guess store in Philadelphia - Pennsylvania
+Since Guess was looking to make its impact once again on the fashion market, the Marciano brothers called upon hotel heiress and fashion mogul Paris Hilton to feature in a new series of ads back in 2009.[citation needed]
+
+Today
+
+Guess retail store on the Magnificent Mile in Chicago
+
+Guess showroom in New York City
+On January 26, 2001, Guess Inc. restated previous results for fiscal 2000 after deciding to write down impaired inventory.[20] In 2004, Guess celebrated the 20th anniversary of its watch collection, issuing a special-edition Guess watch. The accessories department was also greatly expanded and several stores across the United States were redesigned. Guess also created a lower priced collection sold exclusively through its outlet locations. Guess also introduced its first brand extension, the up-scale female line of clothing and accessories, named Marciano. The line features pricey, runway-inspired styles, and is featured in several retail locations throughout the world, as well as select Guess retail stores.[21]
+
+In November 2006, Guess introduced their Marciano men's line, which was available exclusively through Guess.com and select Guess and Marciano retailers throughout the country. The men's collection featured button up shirts, blazers, and dress pants. In line with the women's collection, the men's line was developed and manufactured in Florence, Italy; and retailed for a higher price. As of January 2007, the Marciano men's line was pulled from all Guess and Marciano stores due to lackluster sales. The merchandise was marked down, and was to be sent to factory outlet stores upon deletion.
+
+In 2012, 23 years after first posing for the brand, supermodel Claudia Schiffer posed again for Guess' black-and-white ads marking the brand's 30th anniversary.[22]
+
+In 2015 it was announced that founder Paul Marciano will be stepping down from his position as CEO with Victor Herrero as his carefully selected replacement, in August of that same year.[23]
+
+As of 2017 ex Fifth Harmony member and "Havana" singer Camila Cabello was announced as the new face of Guess.
+For more information please click : <a href="https://en.wikipedia.org/wiki/Guess_(clothing)">here</a>
+</p></p>
+
+<script>
+  jQuery(window).scroll(function(){
+    var vscroll = jQuery(this).scrollTop();
+    jQuery('#logotext').css({
+      "transform": "translate(0px, "+vscroll/2+"px)"
+    });
+
+    var vscroll = jQuery(this).scrollTop();
+    jQuery('#back-flower').css({
+      "transform": "translate("+scroll/5+"px, -"+vscroll/12+"px)"
+    });
+
+    var vscroll = jQuery(this).scrollTop();
+    jQuery('#fore-flower').css({
+      "transform": "translate(0px, -"+vscroll/2+"px)"
+    });
+  });
+</script>
+</body>
+</html>>
