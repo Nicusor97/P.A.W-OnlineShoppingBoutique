85e56e788a4b897095bfb24f79831e01a7aea3bd
DESKTOP-047NQ7S\Nick <nicolaepicatureanu@gmail.com>
2018-05-13 11:31:03 -0700

    improvements

---
+++ OnlineShopping/ContextClass/clsEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ECommPract.ContextClass
+{
+    public class clsEnum
+    {
+        public enum Categories
+        {
+            MAN = 1,
+            WOMAN =2,
+            SPORT = 3,
+            HANDBAG =4,
+            KIDS = 5 
+        }
+    }
+}
---
+++ OnlineShopping/Global.asax.cs
@@ -0,0 +1,47 @@
+﻿using ECommPract.Models;
+using ECommPract.Models.MainClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace ECommPract
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        protected void Session_End()
+        {
+            //ApplicationDbContext db = new ApplicationDbContext();
+            //ShoppingCartActions cart = new ShoppingCartActions();
+            //string _shoppingCartId = cart.GetCartId();
+            //if(_shoppingCartId != User.Identity.Name)
+            //{
+            //    var cartItems = db.SHOPPINGCART.Where(x => x.CartId == _shoppingCartId).ToList();
+            //    db.SHOPPINGCART.RemoveRange(cartItems);
+            //    db.SaveChanges();
+            //}
+        }
+        protected void Application_End()
+        {
+            //ApplicationDbContext db = new ApplicationDbContext();
+            //ShoppingCartActions cart = new ShoppingCartActions();
+            //string _shoppingCartId = cart.GetCartId();
+            //if (_shoppingCartId != User.Identity.Name)
+            //{
+            //    var cartItems = db.SHOPPINGCART.Where(x => x.CartId == _shoppingCartId).ToList();
+            //    db.SHOPPINGCART.RemoveRange(cartItems);
+            //    db.SaveChanges();
+            //}
+        }
+    }
+}
